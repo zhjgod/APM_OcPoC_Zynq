10bb9d0d8e895bbeb55567710fed4b6e0ca1366c
zhjgod <3825089@qq.com>
2018-12-17 15:02:51 +0800

    single

--- libraries/AP_Proximity/AP_Proximity_uSharp3D.h
+++ libraries/AP_Proximity/AP_Proximity_uSharp3D.h
@@ -12,36 +12,6 @@
 #define PROXIMITY_USHARP3D_DISTANCE_MAX      999.0f
 #define PROXIMITY_USHARP3D_DISTANCE_MIN      0.5f
 
-//#define PROTOCAL_STR
-#define PROTOCAL_BYTE
-
-#ifdef PROTOCAL_STR
-struct target_3d {
-	float snr;
-    float dis;
-    float vel;
-    float agl_h;
-    float pow;
-    float agl_v;
-    float agl_v1;
-};
-#endif
-#ifdef PROTOCAL_BYTE
-struct target_3d {
-    float snr;
-    float dis;
-    float vel;
-    float agl_h;
-	float pow;
-    float agl_v;
-	float oid;
-};
-#endif
-
-//struct targets_3d {
-//	target_3d targets[32];
-//	uint8_t count;
-//};
 
 class AP_Proximity_uSharp3D  : public AP_Proximity_Backend
 {
@@ -70,27 +40,8 @@ private:
     AP_HAL::UARTDriver *uart_wifi = nullptr;
 
     uint16_t buf_idx;
-#ifdef PROTOCAL_STR
-    char buf[128];
-    bool end;
-    uint8_t t_idx;
-#endif
-#ifdef PROTOCAL_BYTE
-    uint8_t buf[256];
-#endif
+    uint8_t buf[32];
     uint32_t time_begin = 0;
-
-    target_3d targets[32];
-
-    double roll_FK;
-    double pitch_FK;
-    double yaw_FK;
-    double range_data[32];
-    int range_size[2];
-    double angleV_data[32];
-    int angleV_size[2];
-    double angleH_data[32];
-    int angleH_size[2];
 
     float avoid_dis;
 };
